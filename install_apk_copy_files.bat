47b469f234986d63071086b90dc04194981c3a8e
hepeng <hepeng@skyworth.com>
2014-12-30 12:49:46 +0800

    init project

---
+++ install_apk_copy_files.bat
@@ -0,0 +1,9 @@
+cd 01_adb_tools
+
+adb wait-for-device
+
+:pause
+
+for %%i in (..\11_apk\*.apk) do adb install -r "%%i"
+
+pause
---
+++ start_adb.bat
@@ -0,0 +1,7 @@
+cd 01_adb_tools
+
+adb wait-for-device
+
+adb shell
+
+pause
